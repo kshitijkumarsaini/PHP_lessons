1b3644212265127cc3a0c42290b3c876f02a9a5b
kshitijkumarsaini <72598772+kshitijkumarsaini@users.noreply.github.com>
2021-07-23 18:45:41 +0530

    Add files via upload

---
+++ test.php
@@ -0,0 +1,190 @@
+
+                
+
+            <div id="main" class="site-main">
+                <div id="primary" class="content-area col-md-12 col-lg-12 col-sm-12 col-xs-12">
+                    <div id="content" class="site-content" role="main">
+                        <article id="post-263" class="post-263 page type-page status-publish hentry">
+                            <div class="entry-content">
+							
+								
+
+
+                                <div class="vc_row wpb_row vc_row-fluid vc_custom_1406118725132">
+                                    <div class="section clearfix grid_section ">
+                                        <div class="wpb_column vc_column_container vc_col-sm-7 ">
+                                            <div class="vc_column-inner ">
+                                                <div class="wpb_wrapper">
+                                                    <div class="vc_row wpb_row vc_inner vc_row-fluid">
+                                                        <div class="wpb_column vc_column_container vc_col-sm-12">
+                                                            <div class="vc_column-inner ">
+                                                                <div class="wpb_wrapper">
+                                                                    <section class="kwayy-servicebox kwayy-servicebox-lefticonspacing wpb_animate_when_almost_visible wpb_appear wpb_start_animation none">
+                                                                        <div class="kwayy-servicebox-title-wrapper"><i class="kwayy-icon kwicon-fa-leaf icon-size-large icon-align-left kwayy-ibgcolor kwayy-ibgcolor-skincolor"></i>
+                                                                            <h2 class="kwayy-servicebox-title">Welcome to Skin Mantraa</h2>
+                                                                            <div class="kwayy-servicebox-content">
+                                                                                <p>We are a full spectrum state-of-art dermatology centre and combine medical,surgical and cosmetic dermatology. Our centre gives economical facilities at par with the international standards, giving you a refreshing, relaxed and hygienic environment.</p>
+
+<p>We are here to show you how small changes can make a big difference to your skin. Our approach to skin care is unique, providing an ideal combination of aesthetic and clinical care. To bring you latest techniques, cutting edge technologies and specialist in this field. Most of all, we are here to make your skin, through knowledge and understanding, the very best it can be.</p>
+
+<p>All treatments available at Skin Mantraa are for both male and female and are carried out under strict supervision of Dr. Bhavna Mangla</p>
+                                                                            </div>
+                                                                        </div>
+                                                                    </section>
+                                                                    
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    
+					<div class="wpb_column vc_column_container vc_col-sm-5 ">
+                                            <div class="vc_column-inner ">
+                                                <div class="wpb_wrapper">
+						    <section class="kwayy-servicebox kwayy-servicebox-lefticonspacing wpb_animate_when_almost_visible wpb_appear wpb_start_animation none">
+							<div class="kwayy-servicebox-title-wrapper"><i class="kwayy-icon kwicon-fa-leaf icon-size-large icon-align-left kwayy-ibgcolor kwayy-ibgcolor-skincolor"></i>
+							    <h2 class="kwayy-servicebox-title">About Dr. Bhavna Mangla</h2>
+							    <div class="kwayy-servicebox-content">
+								<p>Dr. Bhavna Mangla is a young and vibrant dermatologist practicing in Delhi.Dr.Bhavna has graduated from Manipal University (M.A.H.E.) and acquired her Post graduation in Dermatology at Jawaharlal Nehru Medical College, Belgaum. She has done her training in Cosmetology and Dermatosurgery at St. Johns Medical College, Bangalore . She has done her fellowship training in Cosmetic surgery at V.M.Medical College and Safdurjang hospital,Delhi.</p>
+								<p>She specializes in the fields of Dermatology, Dermatosurgery, Laser Surgery and Hair Transplantation. </p>
+							    </div>
+							</div>
+						    </section>
+						    
+						</div>
+					    </div>
+                                        </div>
+                                        
+				    </div>
+                                </div>
+
+							   <div data-vc-parallax="1.5" class="vc_row wpb_row vc_row-fluid vc_custom_1452668290399 kwayy-row-bgprecolor-dark kwayy-bg-overlay kwayy-row-parallax vc_row-has-fill vc_general vc_parallax vc_parallax-true">
+                                    <div class="section clearfix grid_section ">
+                                        <div class="wpb_column vc_column_container vc_col-sm-12 ">
+                                            <div class="vc_column-inner ">
+                                                <div class="wpb_wrapper">
+                                                    <div class="row  kwayy-portfolio-boxes-wrapper kwayy-portfolio-view-carousel kwayy-effect-carousel kwayy-items-col-three kwayy-carousel-col-three" id="kwayy-portfolio-id-59695803" data-autoplay="1" data-autoplayspeed="800" data-autoplaytimeout="4500" data-loop="0">
+                                                        <div class="kwayy-portfolio-text col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                                            <header class="kwayy-heading-wrapper kwayy-heading-wrapper-align-center">
+                                                                <div class="kwayy-heading-wrapper-inner">
+                                                                    <h2 class="kwayy-heading-align-center"><span>WE ARE OFFERING SERVICES</span></h2>
+                                                                </div>
+                                                                <div class="kwayy-carousel-controls-inner"><a href="javascript:void(0)" class="kwayy-carousel-prev"><span class="wpb_button"><i class="kwicon-fa-angle-left"></i></span></a><a href="javascript:void(0)" class="kwayy-carousel-next"><span class="wpb_button"><i class="kwicon-fa-angle-right"></i></span></a>
+                                                                </div>
+                                                            </header>
+                                                        </div>
+                                                        <div class="kwayy-portfolio-boxes col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                                            <div class="kwayy-items-wrapper kwayy-carousel-items-wrapper kwayy-portfolio-boxes-inner portfolio-wrapper row">
+                                                                
+                                                                
+								
+								<article class="portfolio-box portfolio-slider-box-width diagnosis medical therapy kwayy-box">
+                                                                    <div class="item">
+                                                                        <div class="item-thumbnail">
+                                                                            <a href="cosmetic_dermatology/advanced_skin_glow_treatment/44"><img width="1280" height="720" src="<?php echo SITE_PATH_IMAGE;?>advanced_skin_glow_treatment.jpg" class="attachment-portfolio-fix-column size-portfolio-fix-column wp-post-image" alt="pic-3" /></a>
+                                                                        </div>
+                                                                        <div class="item-content">
+                                                                            <h4><a href="cosmetic_dermatology/advanced_skin_glow_treatment/44">Advanced Skin Glow Treatment</a></h4>
+                                                                        </div>
+                                                                    </div>
+                                                                </article>
+								<article class="portfolio-box portfolio-slider-box-width diagnosis medical therapy kwayy-box">
+                                                                    <div class="item">
+                                                                        <div class="item-thumbnail">
+                                                                            <a href="services/laser_treatments/pain_free_body_hair_removal/1"><img width="1280" height="720" src="<?php echo SITE_PATH_IMAGE;?>body_hair_removal.jpg" class="attachment-portfolio-fix-column size-portfolio-fix-column wp-post-image" alt="pic-3" /></a>
+                                                                        </div>
+                                                                        <div class="item-content">
+                                                                            <h4><a href="services/laser_treatments/pain_free_body_hair_removal/1"> Body Hair Removal</a></h4>
+                                                                        </div>
+                                                                    </div>
+                                                                </article>
+								<article class="portfolio-box portfolio-slider-box-width diagnosis medical therapy kwayy-box">
+                                                                    <div class="item">
+                                                                        <div class="item-thumbnail">
+                                                                           <a href="services/laser_treatments/acne_pimples_and_oily_skin_treatment/2"> <img width="1280" height="720" src="<?php echo SITE_PATH_IMAGE;?>acne_pimples_and_oily_skin_treatment.jpg" class="attachment-portfolio-fix-column size-portfolio-fix-column wp-post-image" alt="pic-3" /><a/>
+                                                                        </div>
+                                                                        <div class="item-content">
+                                                                            <h4><a href="services/laser_treatments/acne_pimples_and_oily_skin_treatment/2"> Acne (Pimples) and Oily Skin Treatment</a></h4>
+                                                                        </div>
+                                                                    </div>
+                                                                </article>
+								
+                                                               
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+								<div class="vc_row wpb_row vc_row-fluid vc_custom_1406118725132">
+                                    <div class="section clearfix grid_section ">
+                                        <div class="kwayy-heading-wrapper-inner">
+											<h2 class="kwayy-heading-align-center"><span>TESTIMONIALS</span></h2>
+										</div>
+										
+										<div class="wpb_column vc_column_container vc_col-sm-6" id="ritu_video">
+                                           <img src="<?php echo SITE_PATH_IMAGE.'testimonial/ritu.jpg'?>" id="ritu"/>
+										   
+                                        </div>
+										
+										<div class="wpb_column vc_column_container vc_col-sm-6" id="shilphi_video">
+                                           <img src="<?php echo SITE_PATH_IMAGE.'testimonial/shilphi.jpg'?>" id="shilphi"/>
+										   
+                                        </div>
+
+										
+                                        
+									</div>
+									<div class="section clearfix grid_section " style="margin-top:70px;">
+                                        
+										<div class="wpb_column vc_column_container vc_col-sm-6" id="sanjay_video">
+                                           <img src="<?php echo SITE_PATH_IMAGE.'testimonial/sanjay.jpg'?>" id="sanjay"/>
+										   
+                                        </div>
+										
+										<div class="wpb_column vc_column_container vc_col-sm-6" id="HairRegrowthTestimonial_video">
+                                            <img src="<?php echo SITE_PATH_IMAGE.'testimonial/HairRegrowthTestimonial.jpg'?>" id="HairRegrowthTestimonial"/>
+											
+                                        </div>
+                                        
+									</div>
+									
+                                </div>
+
+
+							</div>
+                            <footer class="entry-meta">
+                            </footer>
+                        </article>
+                        <div id="comments" class="comments-area">
+                        </div>
+                    </div>
+                </div>
+            </div>
+			<script>
+			jQuery(document).ready(function() {
+			  jQuery('#sanjay').on('click', function(ev) {
+				jQuery("#sanjay_video").html('<iframe id="video" width="500" height="350" src="//www.youtube.com/embed/HoXyhTWmS2s?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe>');
+				ev.preventDefault();		 
+			  });
+			   jQuery('#shilphi').on('click', function(ev) {
+				jQuery("#shilphi_video").html('<iframe id="video" width="500" height="350" src="//www.youtube.com/embed/eF7xlVC7QAg?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe>');
+				ev.preventDefault();		 
+			  });
+			  jQuery('#HairRegrowthTestimonial').on('click', function(ev) {
+				jQuery("#HairRegrowthTestimonial_video").html('<iframe id="video" width="500" height="350" src="//www.youtube.com/embed/ISSKn_Z8Yvc?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe>');
+				ev.preventDefault();		 
+			  });
+			  jQuery('#ritu_video').on('click', function(ev) {
+				jQuery("#ritu_video").html('<iframe id="video" width="500" height="350" src="//www.youtube.com/embed/lopzcAM-YHE?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe>');
+				ev.preventDefault();		 
+			  });
+			  
+			});
+			</script>
+            
